ec5c3468364a0162a49e9e7f55eac02a5f6aa369
Rohit <59466377+rohitpatil07@users.noreply.github.com>
2025-03-30 15:32:57 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-containers=$(docker ps -a --filter "ancestor=chatanalysis" --format "{{.ID}}")  
-ids=$(docker ps -a --filter "ancestor=chatanalysis" --format "{{.Names}},{{.ID}}")
+
+while getopts "i:" opt; do
+  case $opt in
+    i) image_name="$OPTARG" ;;
+    *) echo "Usage: $0 -i <image_name>"; exit 1 ;;
+  esac
+done
+
+if [[ -z "$image_name" ]]; then
+  echo "Error: Image name is required"
+  echo "Usage: $0 -i <image_name>"
+  exit 1
+fi
+
+containers=$(docker ps -a --filter "ancestor=$image_name" --format "{{.ID}}")  
+ids=$(docker ps -a --filter "ancestor=$image_name" --format "{{.Names}},{{.ID}}")
 
 if [[ -z "$containers" ]]; then
-  echo "No more containers running chatanalysis image"
+  echo "No more containers running $image_name image"
   exit 0
 fi
 
-echo "LOG : Containers running chatanalysis Image"
+echo "LOG : Containers running $image_name Image"
 echo "------------------------------------------"
 echo "NAME,ID"
 for id in $ids; do
